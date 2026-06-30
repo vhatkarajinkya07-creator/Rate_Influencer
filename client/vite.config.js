9eb15192801383db14597d34ca46058d1d26a31f
Ajinkya <vhatkarajinkya07@gmail.com>
2026-06-30 17:49:46 +0530

    fix api routing for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": { target: "http://localhost:5001", changeOrigin: true },
+      "/api": { target: "https://rateinfluencer-api.onrender.com", changeOrigin: true },
     },
   },
 });
