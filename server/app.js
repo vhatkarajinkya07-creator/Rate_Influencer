06b56f5702f343bb71b2dd90872f268e46f264d5
Ajinkya Vhatkar <vhatkarajinkya07@gmail.com>
2026-07-01 02:14:32 +0530

    Update allowedOrigins for CORS configuration

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 */
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://rate-influencer-mu.vercel.app"
+  "https://rate-influencer-git-main-vhatkarajinkya07-creators-projects.vercel.app"
 ];
 
 app.use(cors({
